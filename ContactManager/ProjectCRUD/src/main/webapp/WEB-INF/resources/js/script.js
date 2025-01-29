81e20988a5806858c68de6fdd07f44355010cc45
ravirajpoot2204 <ravirajpoot2204@gmail.com>
2025-01-29 21:51:05 +0530

    Revert "Adding Projects"
    This reverts commit d4c5ce6af2f44e71ab5e3abc88617cffe66f5b65.

--- ContactManager/ProjectCRUD/src/main/webapp/WEB-INF/resources/js/script.js
+++
@@ -1,5 +0,0 @@
-// Add any necessary JavaScript functionality here
-document.addEventListener('DOMContentLoaded', function() {
-    // Example: Display an alert when the page loads
-    console.log('Page loaded successfully!');
-});
--- VideoStreamAppBackend/src/main/java/com/videostream/repository/PlaylistRepository.java
+++
@@ -1,9 +0,0 @@
-package com.videostream.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.videostream.model.PlayLists;
-
-public interface PlaylistRepository extends JpaRepository<PlayLists, String> {
-
-}
